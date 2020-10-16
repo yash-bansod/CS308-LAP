bb4054271d46cb38f294809092e67830550ca910
Yash Bansod <yashbansod33@gmail.com>
2020-10-16 17:41:54 +0000

    NEGSUPPORT force conflict

--- sqrt.c
+++ sqrt.c
@@ -10,7 +10,7 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT: Usage: sqrt input\n\n");
 		exit(-1);
 	}
     if(strlen(argv[1])>0){
@@ -20,17 +20,17 @@ int main(int argc, char* argv[]) {
                     continue;
             }
             if(!isdigit(argv[1][i])){
-                printf("Input not a number\n");
+                printf("NEGSUPPORT: Input not a number\n");
                 exit(-1);
             }
         }
     }
 	int input = atoi(argv[1]);
     if(input>=0)
-	    printf("Sqrt of %d is %f\n",input,sqrt(input));
+	    printf("NEGSUPPORT: Sqrt of %d is %f\n",input,sqrt(input));
     else
-	    printf("Sqrt of %d is %fi\n",input,sqrt(-input));
-    printf("End of program. Exiting\n");
+	    printf("NEGSUPPORT: Sqrt of %d is %fi\n",input,sqrt(-input));
+    printf("NEGSUPPORT: End of program. Exiting\n");
 	return(0);
 
 } // end main
